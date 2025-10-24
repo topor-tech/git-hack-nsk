ea6de6e76f527a1e627639af40f02c70f9665f22
ra_coder <d-sun-d@yandex.ru>
2025-10-24 05:29:04 +0300

    one more try

--- src/front/sfera_pulse/vite.config.ts
+++ src/front/sfera_pulse/vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     allowedHosts: ['sfera.gitcto.space']
   },
   preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
     allowedHosts: ['sfera.gitcto.space']
   }
 })
