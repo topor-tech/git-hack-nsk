9740d615775e191f41626328bfa66fc5758b85eb
ra-coder <d-sun-d@yandex.ru>
2025-10-24 14:43:19 +0300

    Add to dashboard commits diff info (#4)
    * Add deploy 
    
    https://github.com/topor-tech/git-hack-nsk
    
    * change host and scheme of backfor front on prod
    * Add missed
    * one more try
    * One more try
    * Add +1 allow
    * Add missed
    * Fix Dockerfile
    * Fix Dockerfile
    * move front
    * Add Docker for front
    * Fix name error
    * Merge remote-tracking branch 'git-hack-nsk/deploy' into deploy
    * Initial commit
    * Add deploy
    * Initial commit
    
    * Some front polishing #20
    
    * Add keep params of repo in url
    * Merge branch 'master' of https://gateway-codemetrics.saas.sferaplatform.ru/app/sourcecode/api/team16/base_repo into polising
    * save progress
    
    * Add v1 of dashboard #24
    
    * change order
    * Adjust widget
    * Save dashboard progress
    * Add dashboard
    * Add diagram
    * Add readme
    
    * Add more widgets to dashboard
    
    ---------
    
    Co-authored-by: Рябченко Александр <d-sun-d@ya.ru>

--- src/back/sfera_hack/sfera_hack/api/pulse/__init__.py
+++ src/back/sfera_hack/sfera_hack/api/pulse/__init__.py
@@ -5,6 +5,7 @@ from .get_available_projects import router as get_available_projects_router
 from .get_wip_branches import router as get_wip_branches_router
 from .last_commits import router as last_commits_router
 from .get_dashboard import router as get_dashboard_router
+from .get_diffs_dashboard import router as get_diffs_dashboard_router
 
 router = APIRouter(prefix="/pulse", tags=["pulse"])
 
@@ -13,3 +14,4 @@ router.include_router(get_available_repos_router)
 router.include_router(get_wip_branches_router)
 router.include_router(last_commits_router)
 router.include_router(get_dashboard_router)
+router.include_router(get_diffs_dashboard_router)
